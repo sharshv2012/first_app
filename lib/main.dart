4822368dfa860eb7e90f472ad648ca0d7b98fd45
Harsh <sharshvardhan759@gmail.com>
2023-08-19 21:07:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text("hiii"))))); //using cosnt is used to avoid duplicate memory allocation if the widgets are reused it's a clean coding practice in flutter.
+
+} 
+  // scaffolds sets up the screen , it's inside the materialApp
+
+
+
+
+
