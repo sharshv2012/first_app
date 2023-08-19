56b82de2d1c4f5291b5edc55f4e2d77fe5cc66e3
Harsh <sharshvardhan759@gmail.com>
2023-08-19 21:38:37 +0530

    Commit 1 : Understanding Widget Tree , learnt how to set gradient under container Widget and it's alignment too : )

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text("hiii"))))); //using cosnt is used to avoid duplicate memory allocation if the widgets are reused it's a clean coding practice in flutter.
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.pinkAccent,
+                Colors.purpleAccent
+              ],
+              begin: Alignment.topLeft ,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text("hiii"),
+          ),
+        ),
+      ),
+    ),
+  ); //using cosnt is used to avoid duplicate memory allocation if the widgets are reused it's a clean coding practice in flutter.
 } 
   // scaffolds sets up the screen , it's inside the materialApp
 
